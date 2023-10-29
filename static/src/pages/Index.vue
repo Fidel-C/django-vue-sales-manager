c0aab90a6a6146206e6fa3f8c3ab4b0b44bce503
fidelc <fidelchukwunyere@gmail.com>
2023-10-29 14:59:04 +0100

    initial commit

---
+++ static/src/pages/Index.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+
+    <div>This  is a vue app in django developed by {{dev}}</div>
+
+    </div>
+</template>
+
+<script setup lang="ts">
+import { toRefs } from 'vue'
+
+
+
+const props=defineProps(['dev'])
+
+toRefs(props)
+
+
+</script >
+
+<style scoped>
+
+</style>
